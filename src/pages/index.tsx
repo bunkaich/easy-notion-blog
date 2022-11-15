abea867dc33d37a57b1b7dd2e929d39ce0a5e8f7
Kohei Yamaguchi <bunkaich@gmail.com>
2022-11-15 22:55:01 +0900

    change text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,13 +7,13 @@ const RenderPage = () => (
     <DocumentHead />
 
     <div>
-      <h2>このブログについて</h2>
+      <h2>このブログについて:</h2>
       <p>ライフハックが好きな社会人、ぶんかいと申します。</p>
       <p>最近はZettelkastenというメモテクニックを個人的に研究中です。</p>
       <p>
         便利ツールやちょっと生活を便利にする術など投稿したいなと思ってます。
       </p>
-      <h2>アカウント</h2>
+      <h2>アカウント:</h2>
       <p>
         <ExtLink href="https://twitter.com/bunkaich">Twitter</ExtLink>
       </p>
